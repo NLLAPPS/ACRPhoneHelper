2461821b507d93257cb01e906e88b46a87bf395f
NLL <NLL>
2022-05-09 11:28:53 +0100

    More documentation

--- app/src/main/java/com/nll/helper/server/IRemoteServiceListener.kt
+++ app/src/main/java/com/nll/helper/server/IRemoteServiceListener.kt
@@ -3,15 +3,10 @@ package com.nll.helper.server
 import remoter.annotations.Remoter
 
 
+
 /**
  *
- * Server files should be within app-recorder and copied to acr module app-helper-client.
- *
- * Spent a day trying to move client files to its own module like server
- *
- * Had to include server files and consequently included manifest declarations for the server
- *
- * Spend 3 hour figuring out ACR PHone was actually connection to itself because server files were packaged!!
+ *  See https://github.com/josesamuel/remoter
  *
  */
 @Remoter
--- app/src/main/java/com/nll/helper/server/RemoteService.kt
+++ app/src/main/java/com/nll/helper/server/RemoteService.kt
@@ -9,15 +9,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlin.properties.Delegates
 
 /**
- *
- * Server files should be within app-recorder and copied to acr module app-helper-client.
- *
- * Spent a day trying to move client files to its own module like server
- *
- * Had to include server files and consequently included manifest declarations for the server
- *
- * Spend 3 hour figuring out ACR PHone was actually connection to itself because server files were packaged!!
- *
+ * Wrapper for IRemoteService_Proxy
  */
 class RemoteService : LifecycleService() {
     private val logTag = "CR_RemoteService (${Integer.toHexString(System.identityHashCode(this))})"
