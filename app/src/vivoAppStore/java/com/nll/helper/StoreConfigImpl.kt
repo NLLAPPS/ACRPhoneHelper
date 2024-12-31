032883668f88f726d3add456e88ca86ae4c18e7a
nll <nll@nll>
2024-12-31 10:07:51 +0000

    Vivo App Store

---
+++ app/src/vivoAppStore/java/com/nll/helper/StoreConfigImpl.kt
@@ -0,0 +1,36 @@
+package com.nll.helper
+
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+
+object StoreConfigImpl : IStoreConfig {
+    override fun openACRPhoneDownloadLink(context: Context, packageName: String) = try {
+        Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("market://details?id=$packageName")
+        )
+            .let(context::startActivity)
+        true
+
+    } catch (ignored: ActivityNotFoundException) {
+        try {
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://play.google.com/store/apps/details?id=$packageName")
+            )
+                .let(context::startActivity)
+            true
+        } catch (e: ActivityNotFoundException) {
+            e.printStackTrace()
+            false
+        }
+    }
+
+    override fun canLinkToWebSite() = false
+    override fun canLinkToGooglePlayStore() = false
+    override fun getUpdateCheckUrl() = "https://acr.app/version-vivo-app-store.json"
+    override fun requiresProminentPrivacyPolicyDisplay() = true
+    override fun getPrivacyPolicyUrl()= "https://acr.app/policy.htm"
+}
